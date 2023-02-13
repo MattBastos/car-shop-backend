18c1c8e119db22ab99839e5662ad11c887aae9bc
Matthews Bastos <matthewsbasttos@gmail.com>
2023-02-13 17:47:56 -0300

    feat: Cria a classe CarService

---
+++ src/Services/CarService.ts
@@ -0,0 +1,16 @@
+import Car from '../Domains/Car';
+import ICar from '../Interfaces/ICar';
+import CarODM from '../Models/CarODM';
+
+export default class CarService {
+  private createCarDomain(car: ICar | null): Car | null {
+    if (!car) return null;
+    return new Car(car);
+  }
+
+  public async create(car: ICar) {
+    const carODM = new CarODM();
+    const newCar = await carODM.create(car);
+    return this.createCarDomain(newCar);
+  }
+}
