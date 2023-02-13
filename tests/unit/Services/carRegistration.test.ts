1f76d83714fbc9068e6e5c3d9cda24c8b3b2d715
Matthews Bastos <matthewsbasttos@gmail.com>
2023-02-13 16:22:57 -0300

    test: Define os cenários para teste do registro de carros

---
+++ tests/unit/Services/carRegistration.test.ts
@@ -0,0 +1,9 @@
+describe('Car registration', function () {
+  it('Should successfully register a car', function () {
+
+  });
+
+  it('Should throw an exception when the car attributes are invalid', function () {
+
+  });
+});
