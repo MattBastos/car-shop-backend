d5df5497f473e7fd579f6b22dcfddd3b04d238f3
Matthews Bastos <matthewsbasttos@gmail.com>
2023-02-13 21:23:30 -0300

    test: Adiciona teste para o método find

--- tests/unit/Services/getCars.test.ts
+++ tests/unit/Services/getCars.test.ts
@@ -2,10 +2,16 @@ import { expect } from 'chai';
 import sinon from 'sinon';
 import { Model } from 'mongoose';
 import CarService from '../../../src/Services/CarService';
+import { allCars } from '../../mocks/getCarsMock.mock';
 
 describe('Get all cars and get cars by Id', function () {
-  it('Should return a list with all cars', function () {
+  it('Should return a list with all cars', async function () {
+    sinon.stub(Model, 'find').resolves(allCars);
 
+    const carService = new CarService();
+    const result = await carService.find();
+
+    expect(result).to.be.deep.equal(allCars);
   });
 
   it('Should return a car by Id', function () {
