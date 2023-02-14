1651c5dba1c38c260dcea635488aa7e4197334f5
Matthews Bastos <matthewsbasttos@gmail.com>
2023-02-13 21:14:36 -0300

    test: Define os cenários para os testes dos métodos getCars

---
+++ tests/unit/Services/getCars.test.ts
@@ -0,0 +1,22 @@
+import { expect } from 'chai';
+import sinon from 'sinon';
+import { Model } from 'mongoose';
+import CarService from '../../../src/Services/CarService';
+
+describe('Get all cars and get cars by Id', function () {
+  it('Should return a list with all cars', function () {
+
+  });
+
+  it('Should return a car by Id', function () {
+
+  });
+
+  it('Should return an exception if the car does not exist', function () {
+
+  });
+
+  it('Should return an exception if the id is invalid', function () {
+
+  });
+});
