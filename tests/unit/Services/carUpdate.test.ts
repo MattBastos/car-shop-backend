1cdd0528410f4b85bb1a919d9bf6841d1b5c3aaf
Matthews Bastos <matthewsbasttos@gmail.com>
2023-02-15 09:34:00 -0300

    test: Adiciona o teste para o caso em que o carro nao existe

--- tests/unit/Services/carUpdate.test.ts
+++ tests/unit/Services/carUpdate.test.ts
@@ -10,11 +10,18 @@ describe('Update car by id', function () {
   });
 
   it('Should update a car successfully and return its data', async function () {
-
+  
   });
 
   it('Should return an exception if the car does not exists', async function () {
+    sinon.stub(Model, 'findByIdAndUpdate').resolves();
 
+    try {
+      const carService = new CarService();
+      await carService.findByIdAndUpdate('634852326b35b59XXXXXX');
+    } catch (err) {
+      expect((err as Error).message).to.be.equal({ message: 'Car not found' });
+    }
   });
 
   it('Should return an exception if the id is invalid', async function () {
