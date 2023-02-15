0b99f79cfccf1f41cb501266450e5873d974cf84
Matthews Bastos <matthewsbasttos@gmail.com>
2023-02-15 10:18:23 -0300

    feat: Cria o método verifyIdDB na classe CarDataValidation

--- src/Services/validations/CarDataValidation.ts
+++ src/Services/validations/CarDataValidation.ts
@@ -1,8 +1,18 @@
 import { isValidObjectId } from 'mongoose';
+import CarODM from '../../Models/CarODM';
 
 export default class CarDataValidation {
+  private _model = new CarODM();
+
   public validateMongooseId(id: string) {
-    if (!isValidObjectId(id)) return { message: 'Invalid mongo id' };
-    return null;
+    if (isValidObjectId(id)) return null;
+    return { message: 'Invalid mongo id' };
+  }
+
+  public async verifyIdDB(id: string) {
+    const data = await this._model.findById(id);
+
+    if (data) return null;
+    return { message: 'Car not found' };
   }
 }
