f507165b44a9e103f6963960fa349bd8a8ffe0e6
Matthews Bastos <matthewsbasttos@gmail.com>
2023-02-15 15:13:07 -0300

    feat: Cria a classe MotorcycleController e o método create

---
+++ src/Controllers/MotorcycleController.ts
@@ -0,0 +1,32 @@
+import { Request, Response } from 'express';
+import IMotorcycle from '../Interfaces/IMotorcycle';
+import MotorcycleService from '../Services/MotorcycleService';
+
+export default class MotorcycleController {
+  private _req: Request;
+  private _res: Response;
+  private _service: MotorcycleService;
+
+  constructor(req: Request, res: Response) {
+    this._req = req;
+    this._res = res;
+    this._service = new MotorcycleService();
+  }
+
+  public async create() {
+    const { body } = this._req;
+
+    const motorcycle: IMotorcycle = {
+      model: body.model,
+      year: body.year,
+      color: body.color,
+      status: body.status,
+      buyValue: body.buyValue,
+      category: body.category,
+      engineCapacity: body.engineCapacity,
+    };
+
+    const newMotorcycle = await this._service.create(motorcycle);
+    return this._res.status(201).json(newMotorcycle);
+  }
+}
