f3265093219545355a18e602f8753d73120753bc
Matthews Bastos <matthewsbasttos@gmail.com>
2023-02-15 14:43:10 -0300

    feat: Cria a classe abstrata AbstractODM

---
+++ src/Models/AbstractODM.ts
@@ -0,0 +1,32 @@
+import { Model, Schema, model, models, UpdateQuery } from 'mongoose';
+
+export default abstract class AbstractODM<T> {
+  protected model: Model<T>;
+  protected schema: Schema;
+  protected modelName: string;
+
+  constructor(schema: Schema, modelName: string) {
+    this.schema = schema;
+    this.modelName = modelName;
+    this.model = models[this.modelName] || model(this.modelName, this.schema);
+  }
+
+  public async create(vehicle: T) {
+    return this.model.create({ ...vehicle });
+  }
+
+  public async find() {
+    return this.model.find();
+  }
+
+  public async findById(id: string) {
+    return this.model.findById(id);
+  }
+
+  public async findByIdAndUpdate(id: string, vehicle: T) {
+    return this.model.findByIdAndUpdate(
+      { _id: id },
+      { ...vehicle } as UpdateQuery<T>,
+    );
+  }
+}
