be9698f34d8d384ac26a28a5b36e76b3a5352ffb
Matthews Bastos <matthewsbasttos@gmail.com>
2023-02-15 15:00:33 -0300

    test: Cria testes para o método create da classe MotorcycleService

---
+++ tests/unit/Services/motorcycleRegistration.test.ts
@@ -0,0 +1,19 @@
+import { expect } from 'chai';
+import sinon from 'sinon';
+import { Model } from 'mongoose';
+import MotorcycleService from '../../../src/Services/MotorcycleService';
+import {
+  motorcycleRegistrationInput,
+  motorcycleRegistrationOutput,
+} from '../../mocks/motorcycleMock.mock';
+
+describe('Motorcycle registration', function () {
+  it('Should successfully register a motorcycle', async function () {
+    sinon.stub(Model, 'create').resolves(motorcycleRegistrationOutput);
+
+    const motorcycleService = new MotorcycleService();
+    const result = await motorcycleService.create(motorcycleRegistrationInput);
+
+    expect(result).to.be.deep.equal(motorcycleRegistrationOutput);
+  });
+});
