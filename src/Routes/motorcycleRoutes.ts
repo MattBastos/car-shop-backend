706681306500468878f0d868fc5aae15d9fb6b89
Matthews Bastos <matthewsbasttos@gmail.com>
2023-02-16 14:29:13 -0300

    feat: Adiciona a rota get para o método find motorcycles

--- src/Routes/motorcycleRoutes.ts
+++ src/Routes/motorcycleRoutes.ts
@@ -4,5 +4,6 @@ import MotorcycleController from '../Controllers/MotorcycleController';
 const motorcycleRoutes = Router();
 
 motorcycleRoutes.post('/', (req, res) => new MotorcycleController(req, res).create());
+motorcycleRoutes.get('/', (req, res) => new MotorcycleController(req, res).find());
 
 export default motorcycleRoutes;
