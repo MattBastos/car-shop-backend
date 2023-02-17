d6bbf0491986c5388f88285e73f21b4b3a596436
Matthews Bastos <matthewsbasttos@gmail.com>
2023-02-17 13:38:47 -0300

    feat: Implementa a rota put para MotorcycleController

--- src/Routes/motorcycleRoutes.ts
+++ src/Routes/motorcycleRoutes.ts
@@ -6,5 +6,6 @@ const motorcycleRoutes = Router();
 motorcycleRoutes.post('/', (req, res) => new MotorcycleController(req, res).create());
 motorcycleRoutes.get('/', (req, res) => new MotorcycleController(req, res).find());
 motorcycleRoutes.get('/:id', (req, res) => new MotorcycleController(req, res).findById());
+motorcycleRoutes.put('/:id', (req, res) => new MotorcycleController(req, res).findByIdAndUpdate());
 
 export default motorcycleRoutes;
