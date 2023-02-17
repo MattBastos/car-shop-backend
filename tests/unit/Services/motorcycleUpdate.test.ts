14311748664eec45b44a34103d3b7362f3d2f93a
Matthews Bastos <matthewsbasttos@gmail.com>
2023-02-17 13:29:54 -0300

    test: Implementa todos os casos de teste para o método update da classe MotorcycleService

--- tests/unit/Services/motorcycleUpdate.test.ts
+++ tests/unit/Services/motorcycleUpdate.test.ts
@@ -10,14 +10,43 @@ describe('Update motorcycle by id', function () {
   });
 
   it('Should update a motorcycle successfully and return its data', async function () {
+    sinon.stub(Model, 'findByIdAndUpdate').resolves();
+    sinon.stub(Model, 'findById').resolves(motorcycleUpdateOutput);
 
+    const motorcycleService = new MotorcycleService();
+    const result = motorcycleService.findByIdAndUpdate(
+      '634852326b35b59438fbea2f',
+      motorcycleUpdateInput,
+    );
+
+    expect(result).to.be.deep.equal(motorcycleUpdateOutput);
   });
 
   it('Should return an exception if the motorcycle does not exists', async function () {
+    sinon.stub(Model, 'findByIdAndUpdate').resolves();
 
+    try {
+      const motorcycleService = new MotorcycleService();
+      await motorcycleService.findByIdAndUpdate(
+        '634852326b35b59XXXXXXX',
+        motorcycleUpdateInput,
+      );
+    } catch (err) {
+      expect((err as Error).message).to.be.equal({ message: 'Motorcycle not found' });
+    }
   });
 
   it('Should return an exception if the id is invalid', async function () {
+    sinon.stub(Model, 'findByIdAndUpdate').resolves();
 
+    try {
+      const motorcycleService = new MotorcycleService();
+      await motorcycleService.findByIdAndUpdate(
+        '634852326b35b59XXXXXXX',
+        motorcycleUpdateInput,
+      );
+    } catch (err) {
+      expect((err as Error).message).to.be.equal({ message: 'Invalid mongo id' });
+    }
   });
 });
