b809701347e7d907787a4ce27a3941a537d539d6
Matthews Bastos <matthewsbasttos@gmail.com>
2023-05-06 10:27:08 -0300

    test: Adiciona casos de testes para o método findByIdAndDelete da classe CarService

---
+++ tests/unit/Services/deleteCar.ts
@@ -0,0 +1,28 @@
+import { expect } from 'chai';
+import sinon from 'sinon';
+import { Model } from 'mongoose';
+import CarService from '../../../src/Services/CarService';
+
+describe('Delete car by id', function () {
+  afterEach(function () {
+    sinon.restore();
+  });
+
+  it('Should delete a car successfully', async function () {
+    sinon.stub(Model, 'findByIdAndDelete').resolves();
+    
+    const carService = new CarService();
+    await carService.findByIdAndDelete('634852326b35b59438fbea2f');
+  });
+
+  it('Should return an exception if the car does not exists', async function () {
+    sinon.stub(Model, 'findByIdAndDelete').resolves();
+
+    try {
+      const carService = new CarService();
+      await carService.findByIdAndDelete('634852326b35b59XXXXXX');
+    } catch (err) {
+      expect((err as Error).message).to.be.equal({ message: 'Car not found' });
+    }
+  });
+});
