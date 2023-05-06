c9e069c140335cc6d1a5fa1fc959a9610e85c891
Matthews Bastos <matthewsbasttos@gmail.com>
2023-05-06 10:38:36 -0300

    refactor: Adiciona um expect no teste de sucesso

--- tests/unit/Services/deleteCar.test.ts
+++ tests/unit/Services/deleteCar.test.ts
@@ -12,7 +12,8 @@ describe('Delete car by id', function () {
     sinon.stub(Model, 'findByIdAndDelete').resolves();
     
     const carService = new CarService();
-    await carService.findByIdAndDelete('634852326b35b59438fbea2f');
+    const result = await carService.findByIdAndDelete('634852326b35b59438fbea2f');
+    expect(result).to.be.deep.equal({ message: 'Car deleted: 634852326b35b59438fbea2f' });
   });
 
   it('Should return an exception if the car does not exists', async function () {
