212341ea3bce54a868e1674d91f67cb7a449ff71
Matthews Bastos <matthewsbasttos@gmail.com>
2023-05-29 14:35:20 -0300

    test: Adiciona testes para o método findByIdAndDelete da classe MotorcycleService

---
+++ tests/unit/Services/Motorcycle/deleteMotorcycle.test.ts
@@ -0,0 +1,29 @@
+import { expect } from 'chai';
+import sinon from 'sinon';
+import { Model } from 'mongoose';
+import MotorcycleService from '../../../../src/Services/MotorcycleService';
+
+describe('Delete motorcycle by id', function () {
+  afterEach(function () {
+    sinon.restore();
+  });
+
+  it('Should delete a motorcycle successfully', async function () {
+    sinon.stub(Model, 'findByIdAndDelete').resolves();
+    
+    const motorcycleService = new MotorcycleService();
+    const result = await motorcycleService.findByIdAndDelete('634852326b35b59438fbea2f');
+    expect(result).to.be.deep.equal({ message: 'Motorcycle deleted: 634852326b35b59438fbea2f' });
+  });
+
+  it('Should return an exception if the motorcycle does not exists', async function () {
+    sinon.stub(Model, 'findByIdAndDelete').resolves();
+
+    try {
+      const motorcycleService = new MotorcycleService();
+      await motorcycleService.findByIdAndDelete('634852326b35b59XXXXXX');
+    } catch (err) {
+      expect((err as Error).message).to.be.equal({ message: 'Motorcycle not found' });
+    }
+  });
+});
