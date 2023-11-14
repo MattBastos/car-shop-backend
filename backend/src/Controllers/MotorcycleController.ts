450ee8861a279f35ff92986553615517389324cf
Matthews Bastos <matthewsbasttos@gmail.com>
2023-11-14 18:28:24 -0300

    refactor: Changes MotorcycleController

--- backend/src/Controllers/MotorcycleController.ts
+++ backend/src/Controllers/MotorcycleController.ts
@@ -2,21 +2,22 @@ import { Request, Response } from 'express';
 import IMotorcycle from '../Interfaces/IMotorcycle';
 import MotorcycleService from '../Services/MotorcycleService';
 
+const INVALID_MONGOOSE_ID_MESSAGE = 'Invalid Mongo Id!';
+const MOTORCYCLE_NOT_FOUND_MESSAGE = 'Motorcycle not found!';
+
 export default class MotorcycleController {
-  private _req: Request;
-  private _res: Response;
-  private _service: MotorcycleService;
-  private _motorcycleNotFoundMessage = 'Motorcycle not found';
-  private _invalidMongoIdMessage = 'Invalid mongo id';
+  private req: Request;
+  private res: Response;
+  private service: MotorcycleService;
 
   constructor(req: Request, res: Response) {
-    this._req = req;
-    this._res = res;
-    this._service = new MotorcycleService();
+    this.req = req;
+    this.res = res;
+    this.service = new MotorcycleService();
   }
 
   public async create() {
-    const { body } = this._req;
+    const { body } = this.req;
 
     const motorcycle: IMotorcycle = {
       model: body.model,
@@ -28,44 +29,58 @@ export default class MotorcycleController {
       engineCapacity: body.engineCapacity,
     };
 
-    const newMotorcycle = await this._service.create(motorcycle);
-    return this._res.status(201).json(newMotorcycle);
+    const newMotorcycle = await this.service.create(motorcycle);
+    return this.res.status(201).json(newMotorcycle);
   }
 
   public async find() {
-    const allMotorcycles = await this._service.find();
-    return this._res.status(200).json(allMotorcycles);
+    const allMotorcycles = await this.service.find();
+    return this.res.status(200).json(allMotorcycles);
   }
 
   public async findById() {
-    const { id } = this._req.params;
-    const { message } = await this._service.findById(id);
+    const { id } = this.req.params;
+    const { message } = await this.service.findById(id);
 
-    if (message === this._motorcycleNotFoundMessage) return this._res.status(404).json({ message });
+    if (message === MOTORCYCLE_NOT_FOUND_MESSAGE) {
+      return this.res.status(404).json(message);
+    }
 
-    if (message === this._invalidMongoIdMessage) return this._res.status(422).json({ message });
+    if (message === INVALID_MONGOOSE_ID_MESSAGE) {
+      return this.res.status(422).json(message);
+    }
 
-    return this._res.status(200).json(message);
+    return this.res.status(200).json(message);
   }
 
   public async findByIdAndUpdate() {
-    const { id } = this._req.params;
-    const { body } = this._req;
-    const { message } = await this._service.findByIdAndUpdate(id, body);
-    
-    if (message === this._motorcycleNotFoundMessage) return this._res.status(404).json({ message });
+    const { id } = this.req.params;
+    const { body } = this.req;
+    const { message } = await this.service.findByIdAndUpdate(id, body);
 
-    if (message === this._invalidMongoIdMessage) return this._res.status(422).json({ message });
+    if (message === MOTORCYCLE_NOT_FOUND_MESSAGE) {
+      return this.res.status(404).json(message);
+    }
 
-    return this._res.status(200).json(message);
+    if (message === INVALID_MONGOOSE_ID_MESSAGE) {
+      return this.res.status(422).json(message);
+    }
+
+    return this.res.status(200).json(message);
   }
 
   public async findByIdAndDelete() {
-    const { id } = this._req.params;
-    const { message } = await this._service.findByIdAndDelete(id);
+    const { id } = this.req.params;
+    const { message } = await this.service.findByIdAndDelete(id);
 
-    if (message === this._motorcycleNotFoundMessage) return this._res.status(404).json({ message });
+    if (message === MOTORCYCLE_NOT_FOUND_MESSAGE) {
+      return this.res.status(404).json(message);
+    }
 
-    return this._res.status(200).json(message);
+    if (message === INVALID_MONGOOSE_ID_MESSAGE) {
+      return this.res.status(422).json(message);
+    }
+
+    return this.res.status(200).json(message);
   }
 }
