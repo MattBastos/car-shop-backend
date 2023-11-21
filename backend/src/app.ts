ee7deb904e73d2835a14c89e4fc7ce99b94922cb
Matthews Bastos <matthewsbasttos@gmail.com>
2023-11-21 14:23:46 -0300

    build: Install cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express';
+import cors from 'cors';
 import carRoutes from './Routes/carRoutes';
 import motorcycleRoutes from './Routes/motorcycleRoutes';
 
 const app = express();
 
+app.use(cors);
 app.use(express.json());
 app.use('/cars', carRoutes);
 app.use('/motorcycles', motorcycleRoutes);
